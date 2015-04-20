88725cc0364a52cc423b8c3dc5576dfa46e218c4
Mimi Lee <mimslee@Mimis-MacBook-Pro.local>
2015-04-20 14:22:16 -0400

    database issues

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import sqlite3
+import sqlite3, datetime, os.path
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
+from flask.ext.wtf import Form
+from wtforms import TextField, TextAreaField, SubmitField, DateTimeField
+from wtforms.validators import DataRequired
 
-     
+
 # CONFIG DATABASE
-DATABASE = '/tmp/flaskr.db'
+DATABASE = 'tmp/todo.db'
 DEBUG = True
 SECRET_KEY = 'pst_key'
 USERNAME = 'admin'
@@ -13,6 +16,7 @@ PASSWORD = 'default'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 # SETTING UP THE DB
@@ -35,18 +39,35 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+# FORM VALIDATION
+# class AddTodo(Form):
+#     title = TextField('Title')
+#     descr = TextAreaField('Description')
+#     dline = DateTimeField('Deadline')
+#     submit = SubmitField('Add')
+
 # ROUTES
 @app.route("/")
 def index():
-    cur = g.db.execute('select id, title, descr, done from entries order by id desc')
-    tasks = [dict(id=row[0], title=row[1], descr=row[2], done=row[3]) for row in cur.fetchall()]
+    # form = AddTodo(request.form)
+    cur = g.db.execute('select id, title, descr, done, dline, finish from entries order by id desc')
+    tasks = [dict(id=row[0], title=row[1], descr=row[2], done=row[3], dline=row[4], finish=row[5]) 
+        for row in cur.fetchall()]
     return render_template('index.html', tasks=tasks)
 
-@app.route('/add', methods=['POST'])
+@app.route('/add', methods=['GET','POST'])
 def add_entry():
-    g.db.execute('insert into entries (title, descr, done) values (?, ?, ?)', [request.form['title'], request.form['descr'], 0])
+    # form = AddTodo(request.form)
+    # print form.title.data
+    # if form.validate():
+    g.db.execute('insert into entries (title, descr, done, dline, finish) values (?, ?, ?, ?, ?)', 
+    [request.form['title'], request.form['descr'], 0, request.form['dline'], datetime.datetime.now().strftime('%Y-%m-%d %H:%M')])
     g.db.commit()
     return redirect(url_for('index'))
+    # else:
+        # flash('Sorry, invalid format. Correct format includes non-empty task name and deadline of format of YYYY-MM-DD HH:MM')
+        # return redirect(url_for('index'))
+
 
 @app.route('/delete', methods=['POST'])
 def delete_task():
@@ -56,6 +77,7 @@ def delete_task():
 
 @app.route('/move', methods=['POST'])
 def move_task():
+    time = datetime.datetime.now()
     g.db.execute('update entries set done=1 where id=?', [request.form['task_to_move']])
     g.db.commit()
     return redirect(url_for('index'))
@@ -63,3 +85,15 @@ def move_task():
 # INITIALIZATION
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
--- todo.sql
+++ todo.sql
@@ -3,5 +3,7 @@ create table entries (
   id integer primary key autoincrement,
   title text not null,
   descr text not null,
-  done integer not null
+  done integer not null,
+  dline datetime,
+  finish timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
